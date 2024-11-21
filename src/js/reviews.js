25fa1598ae0951b979dcc2de6d204fc150dccb2a
Igor <pa220282kiv@gmail.com>
2024-11-22 00:23:05 +0200

    create function launchSwiper

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,12 +1,20 @@
+import Swiper from 'swiper';
+import { Keyboard, Mousewheel, Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/keyboard';
+import 'swiper/css/mousewheel';
+
 import axios from 'axios';
 
-export async function requestReviews() {
+export async function loadReviews() {
   await axios
     .get('https://portfolio-js.b.goit.study/api/reviews')
     .then(response => {
-      createGallery(response.data);      
+      createGallery(response.data);
     })
-    .catch(error => console.log(error));    
+    .catch(error => console.log(error));
 }
 
 function createGallery(data) {
@@ -25,7 +33,31 @@ function createGallery(data) {
         </div>
       </li>`
     )
-    .join('');  
-  gallery.insertAdjacentHTML('beforeend', markup);  
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
 
+export function launchSwiper() {  
+  const swiper = new Swiper('.swiper', {    
+    modules: [Navigation, Pagination, Keyboard, Mousewheel],
+    cssMode: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+      pageUpDown: true,
+    },
+    mousewheel: {
+      enabled: true,
+    },
+    slidesPerView: 2,
+    spaceBetween: 32,
+  });
+}
