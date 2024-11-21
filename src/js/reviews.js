e444095f8354de4c57add75b624525194c9633fd
Igor <pa220282kiv@gmail.com>
2024-11-22 00:14:21 +0200

    add axios

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -3,8 +3,29 @@ import axios from 'axios';
 export async function requestReviews() {
   await axios
     .get('https://portfolio-js.b.goit.study/api/reviews')
-    .then(response => console.log(response))
-    .catch(error => console.log(error));
+    .then(response => {
+      createGallery(response.data);      
+    })
+    .catch(error => console.log(error));    
 }
 
+function createGallery(data) {
+  const gallery = document.querySelector('.swiper-wrapper');
+  console.log(data);
+  let markup = data
+    .map(
+      slide => `
+    <li class="swiper-slide">
+        <p class="review-text">
+          ${slide.review}
+        </p>
+        <div class="review-thumb">
+          <img class="review-avatar" src="${slide.avatar_url}" alt="Autor: ${slide.author}">
+          <p class="review-author">${slide.author}</p>
+        </div>
+      </li>`
+    )
+    .join('');  
+  gallery.insertAdjacentHTML('beforeend', markup);  
+}
 
